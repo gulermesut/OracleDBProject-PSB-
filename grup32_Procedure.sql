09ec5455ffd48e3b336db64b57e5c3a559395a56
gulermesut <77380910+gulermesut@users.noreply.github.com>
2021-01-15 20:46:11 +0300

    Add files via upload

---
+++ grup32_Procedure.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  File created - Perþembe-Ocak-14-2021   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure SUBE_EKLE
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "SYSTEM"."SUBE_EKLE" (sube_no IN VARCHAR,
+                                        sube_adi IN VARCHAR) AS
+BEGIN
+    INSERT INTO SUBELER (SUBENO,SUBEADI) 
+    VALUES (sube_no,sube_adi);
+    COMMIT;
+END;
+
+/
